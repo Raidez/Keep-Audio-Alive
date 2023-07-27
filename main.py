d4c695fba053ab7308d01cdd9a2ad8442a946328
Raidez <alexis.lucien@outlook.fr>
2023-07-27 22:12:11 +0200

    First commit.

---
+++ main.py
@@ -0,0 +1,36 @@
+import os, sys
+from pathlib import Path
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"  # disable import pygame message
+
+import pygame
+
+def resource(path: str):
+    base_path = Path(getattr(sys, '_MEIPASS', '.'))
+    return base_path / path
+
+print("Start silence music")
+
+pygame.mixer.init()
+pygame.mixer.music.load(resource("silence.ogg"))
+pygame.mixer.music.play(loops=-1)
+
+clock = pygame.time.Clock()
+start_time = pygame.time.get_ticks()
+
+try:
+    while pygame.mixer.music.get_busy():
+        clock.tick(10)  # tick every 10 ms
+        time = pygame.time.get_ticks() - start_time
+
+        time_seconds = int(time / 1000 % 60)
+        time_minutes = int(time / 1000 / 60 % 60)
+        time_hours = int(time / 1000 / 60 / 60 % 60)
+        print(
+            f"Playing since {time_hours:02}:{time_minutes:02}:{time_seconds:02}",
+            end="\r",
+        )
+
+except KeyboardInterrupt:
+    print(flush=True)
+    print("Stop silence music")
